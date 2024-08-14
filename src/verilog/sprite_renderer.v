3a17753fc0205d5e36a372687352dee38f92a91a
Reece Laredo-Marcovitz <reece.lm@gmail.com>
2024-08-13 22:28:22 -0400

    Bug fixes, updated to sync ROM

--- src/verilog/sprite_renderer.v
+++ src/verilog/sprite_renderer.v
@@ -15,53 +15,55 @@ module sprite_renderer(
 	output [7:0] vga_b
 );
 
-	parameter SPRITE_HEIGHT = 8;
-	parameter SPRITE_WIDTH = 8;
-	parameter ADDR_WIDTH = 6;
+	parameter SPRITE_HEIGHT = 16;
+	parameter SPRITE_WIDTH = 32;
+	parameter ADDR_WIDTH = 10;
 	parameter SPRITE_PIXELS = SPRITE_HEIGHT*SPRITE_WIDTH;
 	
 	wire line_active;
+	wire sprite_active;
 	
 	wire [COLOR_DEPTH-1:0] default_color;
 	wire [COLOR_DEPTH-1:0] color_code;
 	wire [COLOR_DEPTH-1:0] color_draw;
 	
-	reg [9:0] xcoord_reg;
-	reg [9:0] ycoord_reg;
-	
-	assign color_draw = choose_default ? default_color : color_code;
-	
 	assign default_color = (ycoord >= SCREEN_HEIGHT - FLOOR_HEIGHT) ? COLOR_BLACK_CODE : COLOR_WHITE_CODE;
 	
 	assign line_active = (ycoord >= SCREEN_HEIGHT - FLOOR_HEIGHT - SPRITE_HEIGHT) & (ycoord < SCREEN_HEIGHT-FLOOR_HEIGHT);
-	assign sprite_active = (xcoord >= sprite_position) & (xcoord < sprite_position + SPRITE_WIDTH);
+	assign sprite_active = (hcount >= sprite_position+H_SYNC_WAIT + H_BACK_PORCH) & (hcount < sprite_position + SPRITE_WIDTH+H_SYNC_WAIT + H_BACK_PORCH);//(xcoord >= sprite_position) & (xcoord < sprite_position + SPRITE_WIDTH); //(hcount >= sprite_position+H_SYNC_WAIT + H_BACK_PORCH) & (hcount < sprite_position + SPRITE_WIDTH+H_SYNC_WAIT + H_BACK_PORCH);
 	wire choose_default;
 	assign choose_default = ~(line_active & sprite_active);
+	assign color_draw = choose_default ? default_color : color_code;
 	
 	reg [2**ADDR_WIDTH-1:0] pixel_select;
 
 	always@(posedge clk, negedge reset) begin
 		if(reset == 1'b0) begin
 			pixel_select <= 0;
-			xcoord_reg <= 0;
-			ycoord_reg <= 0;
 		end else begin
 			pixel_select <= (line_active & sprite_active) ? pixel_select + 1 : (pixel_select >= SPRITE_PIXELS - 1 ? 0 : pixel_select);
-			xcoord_reg <= line_active ? xcoord : 0;
-			ycoord_reg <= ycoord;
 		end
 	end
 	
-	// sprite drawing algorithm
-	// At the start of each line (hcount = 0), check if the sprite is on this line (ycoord)
-	// If not, skip (draw default colour/floor)
-	// Otherwise, 
+	// Current issue: Due to clocked ROM, the first pixel gets duplicated twice
+	// Since at the first pixel, xcoord is 0
+	// Solution 1: register the address instead of the output?
+	// Solution 2: line_active goes early one x coordinate earlier?
+	
+	parameter ACCESS_OFFSET = 2;
 	
 	sprite_rom r0(
 		.clk(clk),
-		.addr(pixel_select),
+		.addr((ycoord-(SCREEN_HEIGHT-FLOOR_HEIGHT-SPRITE_HEIGHT))* SPRITE_WIDTH + (xcoord-sprite_position + ACCESS_OFFSET)), //pixel_select
 		.pixel(color_code)
 	);
+	
+	/*sprite_rom_v2 r0(
+		.clk(clk),
+		.addr_x(xcoord-sprite_position),
+		.addr_y(SCREEN_HEIGHT-FLOOR_HEIGHT-1-ycoord),
+		.pixel(color_code)
+	);*/
 	
 	color_select c0(
 		.pix(color_draw),
@@ -81,10 +83,10 @@ module color_select(
 	reg [23:0] rgb;
 	always@(*) begin
 		case(pix)
-			COLOR_WHITE_CODE: rgb <= COLOR_WHITE;
 			COLOR_BLACK_CODE: rgb <= COLOR_BLACK;
 			COLOR_RED_CODE: rgb <= COLOR_RED;
 			COLOR_BLUE_CODE: rgb <= COLOR_BLUE;
+			COLOR_WHITE_CODE: rgb <= COLOR_WHITE;
 		endcase
 	end
 	
@@ -94,24 +96,25 @@ module color_select(
 endmodule
 
 module sprite_rom#(
-	parameter ADDR_WIDTH = 6,
-	parameter NUM_PIXELS = 8*8
+	parameter ADDR_WIDTH = 10
 )(
 	input clk,
 	input [ADDR_WIDTH-1:0] addr,
 	output [COLOR_DEPTH-1:0] pixel
 );
 
-	reg [COLOR_DEPTH-1:0] spr [0:NUM_PIXELS-1];
+	reg [COLOR_DEPTH-1:0] spr [0:(2**ADDR_WIDTH)-1] /* synthesis ramstyle = M10K*/;
 	initial begin
 		$readmemh("sprite_rom.hex", spr);
 	end
 
 	reg [COLOR_DEPTH-1:0] pixel_reg;
+	reg [ADDR_WIDTH-1:0] addr_reg;
 	
-	/*always@(posedge clk) begin
+	always@(posedge clk) begin
 		pixel_reg <= spr[addr];
-	end*/
-	assign pixel = spr[addr];//pixel_reg;
+		//addr_reg <= addr;
+	end
+	assign pixel = pixel_reg;
 	
 endmodule
--- src/verilog/vga_top.v
+++ src/verilog/vga_top.v
@@ -13,39 +13,49 @@ module vga_top(
 
 	reg [9:0] hcount;
 	reg [9:0] vcount;
+	reg [9:0] xcoord_reg;
+	reg [9:0] ycoord_reg;
 	reg hsync_reg;
 	reg vsync_reg;
 	wire vcount_enable;
-	wire active_region;
+	//wire active_region;
+	reg active_region;
+	reg active_region_early;
 	
 	assign vga_sync = ~active_region;
 	
-	assign active_region = (hcount >= H_SYNC_WAIT + H_BACK_PORCH-1) & (vcount >= V_SYNC_WAIT+V_BACK_PORCH-1) & (hcount < H_SYNC_WAIT + H_BACK_PORCH + SCREEN_WIDTH -1) & (vcount < V_SYNC_WAIT+V_BACK_PORCH + SCREEN_HEIGHT-1);
+	//assign active_region = (hcount >= H_SYNC_WAIT + H_BACK_PORCH-1) & (vcount >= V_SYNC_WAIT+V_BACK_PORCH-1) & (hcount < H_SYNC_WAIT + H_BACK_PORCH + SCREEN_WIDTH -1) & (vcount < V_SYNC_WAIT+V_BACK_PORCH + SCREEN_HEIGHT-1);
 	
 	always@(posedge clk, negedge reset) begin
 		if(reset == 1'b0) begin
 			hcount <= 0;
 			vcount <= 0;
+			xcoord_reg <= 0;
+         ycoord_reg <= 0;
 			hsync_reg <= 1'b1;
 			vsync_reg <= 1'b1;
-			//active_region <= 1'b0;
+			active_region <= 1'b0;
+			active_region_early <= 1'b0;
 		end else begin
-			hcount <= (hcount >= LINE_WAIT - 1) ? 0 : hcount + 1;
+			hcount <= (hcount >= LINE_WAIT) ? 0 : hcount + 1;
 			if(vcount_enable == 1'b1) begin
-				vcount <= (vcount >= V_LINES_WAIT - 1) ? 0 : vcount + 1;
+				vcount <= (vcount >= V_LINES_WAIT) ? 0 : vcount + 1;
 			end
-			hsync_reg <= ~(hcount <= H_SYNC_WAIT - 1);
-			vsync_reg <= ~(vcount <= V_SYNC_WAIT - 1);
-			//active_region <= (hcount < WIDTH + H_BACK_PORCH-1) & (vcount < HEIGHT+V_BACK_PORCH-1) & (hcount >= H_BACK_PORCH-1) & (vcount >= V_BACK_PORCH-1);
+			hsync_reg <= ~(hcount <= H_SYNC_WAIT);
+			vsync_reg <= ~(vcount <= V_SYNC_WAIT);
+			xcoord_reg <= hcount - (H_SYNC_WAIT + H_BACK_PORCH);/*active_region ? hcount - (H_SYNC_WAIT + H_BACK_PORCH) : 0;*/
+			ycoord_reg <= vcount - (V_SYNC_WAIT + V_BACK_PORCH);
+			active_region <= (hcount >= H_SYNC_WAIT + H_BACK_PORCH - 1) & (vcount >= V_SYNC_WAIT+V_BACK_PORCH - 1) & (hcount < H_SYNC_WAIT + H_BACK_PORCH + SCREEN_WIDTH - 1) & (vcount < V_SYNC_WAIT+V_BACK_PORCH + SCREEN_HEIGHT - 1);
+			active_region_early <= (hcount >= H_SYNC_WAIT + H_BACK_PORCH - 2) & (vcount >= V_SYNC_WAIT+V_BACK_PORCH - 1) & (hcount < H_SYNC_WAIT + H_BACK_PORCH + SCREEN_WIDTH - 2) & (vcount < V_SYNC_WAIT+V_BACK_PORCH + SCREEN_HEIGHT - 1);
 		end
 	end
 	
-	assign vcount_enable = hcount == LINE_WAIT-1;
+	assign vcount_enable = hcount == LINE_WAIT;
 	assign hsync = hsync_reg;
 	assign vsync = vsync_reg;
 	assign display_en = active_region;
-	assign xcoord = active_region ? hcount - (H_SYNC_WAIT + H_BACK_PORCH - 1) : 0;
-	assign ycoord = active_region ? vcount - (V_SYNC_WAIT + V_BACK_PORCH - 1) : 0;
+	assign xcoord = xcoord_reg; //active_region ? hcount - (H_SYNC_WAIT + H_BACK_PORCH - 1) : 0;
+	assign ycoord = ycoord_reg; //active_region ? vcount - (V_SYNC_WAIT + V_BACK_PORCH - 1) : 0;
 	assign hcount_out = hcount;
 	assign vcount_out = vcount;
 	
